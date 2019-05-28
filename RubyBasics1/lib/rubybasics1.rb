608f30b557853d32b4116b8dc2d682d5db8914c8
Phillip Hirsch <phirsch@uncc.edu>
2019-05-28 16:00:46 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,15 @@
+# Lab 1
+# Part I
+def sum arr
+  return arr.inject(0) { |sum, x| sum + x }
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
