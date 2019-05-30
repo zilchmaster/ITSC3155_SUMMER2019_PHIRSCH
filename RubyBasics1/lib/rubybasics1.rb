9deefcd96b20b0d1687b79d4b49b6e5385589286
Phillip Hirsch <phirsch@uncc.edu>
2019-05-30 15:40:53 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -13,4 +13,20 @@ end
 # Part III
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if (arr.length == 0)
+    return false
+  end
+  if (arr.length == 1)
+    return false
+  end
+  
+  for x in (0..(arr.length-2))
+    for y in ((x+1)...arr.length)
+      
+     return true if arr[x]+arr[y] == n
+        
+    end
+  end
+  
+  return false
 end
