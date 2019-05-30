7b1c9154e9d00b76aef85546e1724a3f7abe5f38
Phillip Hirsch <phirsch@uncc.edu>
2019-05-30 16:08:20 +0000

    Initial RubyHashes Commit

---
+++ RubyHashes/spec/hashes_spec.rb
@@ -0,0 +1,62 @@
+require 'hashes.rb'
+
+RSpec.configure do |config|
+  config.filter_run_excluding :disabled => true
+end
+
+describe 'Ruby Hashes Part I' do
+
+  describe "array_2_hash" do
+    it "should be defined" do
+      expect { array_2_hash(["bobsmith@example.com","sallyfield@example.com","markdole@example.com"], {'Bob Smith'=>'', 'Sally Field'=>'', 'Mark Dole'=>''}) }.not_to raise_error
+    end
+    
+    it "returns the correct hash [20 points]" , points: 20 do
+      expect(array_2_hash(["bobsmith@example.com","sallyfield@example.com","markdole@example.com"], {'Bob Smith'=>'', 'Sally Field'=>'', 'Mark Dole'=>''})).to be_a_kind_of Hash
+      expect(array_2_hash(["bobsmith@example.com","sallyfield@example.com","markdole@example.com"], {'Bob Smith'=>'', 'Sally Field'=>'', 'Mark Dole'=>''})).to eq({'Bob Smith'=>'bobsmith@example.com', 'Sally Field'=>'sallyfield@example.com', 'Mark Dole'=>'markdole@example.com'})
+    end
+    
+    it "works on the empty array [10 points]" , points: 10 do
+      expect { array_2_hash([], {'Bob Smith'=>'', 'Sally Field'=>'', 'Mark Dole'=>''}) }.not_to raise_error
+      expect(array_2_hash([], {'Bob Smith'=>'', 'Sally Field'=>'', 'Mark Dole'=>''})).to eq({'Bob Smith'=>'', 'Sally Field'=>'', 'Mark Dole'=>''})
+    end
+  end
+end
+
+describe 'Ruby Hashes Part II' do
+
+  describe "array2d_2_hash" do
+    it "should be defined" do
+      expect { array2d_2_hash([["bobsmith@example.com", "555-555-5555"],["sallyfield@example.com","111-111-1111"]], {'Bob Smith'=>{}, 'Sally Field'=>{}}) }.not_to raise_error
+    end
+    
+    it "returns the correct hash [30 points]" , points: 30 do
+      expect(array2d_2_hash([["bobsmith@example.com", "555-555-5555"],["sallyfield@example.com","111-111-1111"]], {'Bob Smith'=>{}, 'Sally Field'=>{}})).to be_a_kind_of Hash
+      expect(array2d_2_hash([["bobsmith@example.com", "555-555-5555"],["sallyfield@example.com","111-111-1111"]], {'Bob Smith'=>{}, 'Sally Field'=>{}})).to eq({:"Bob Smith"=>{:email=>"bobsmith@example.com", :phone=>"555-555-5555"}, :"Sally Field"=>{:email=>"sallyfield@example.com", :phone=>"111-111-1111"}})
+     end
+    
+    it "works on the empty array [10 points]" , points: 10 do
+      expect { array2d_2_hash([[]], {'Bob Smith'=>{}, 'Sally Field'=>{}}) }.not_to raise_error
+      expect(array2d_2_hash([[]], {'Bob Smith'=>{}, 'Sally Field'=>{}})).to eq({:"Bob Smith"=>{}, :"Sally Field"=>{}})
+    end
+  end
+end
+
+describe 'Ruby Hashes Part III' do
+
+  describe "hash_2_array" do
+    it "should be defined" do
+      expect { hash_2_array({:"Bob Smith"=>{:email=>"bobsmith@example.com", :phone=>"555-555-5555"}, :"Sally Field"=>{:email=>"sallyfield@example.com", :phone=>"111-111-1111"}}) }.not_to raise_error
+    end
+    
+    it "returns the correct array of arrays [20 points]" , points: 20 do
+      expect(hash_2_array({:"Bob Smith"=>{:email=>"bobsmith@example.com", :phone=>"555-555-5555"}, :"Sally Field"=>{:email=>"sallyfield@example.com", :phone=>"111-111-1111"}})).to be_a_kind_of Array
+      expect(hash_2_array({:"Bob Smith"=>{:email=>"bobsmith@example.com", :phone=>"555-555-5555"}, :"Sally Field"=>{:email=>"sallyfield@example.com", :phone=>"111-111-1111"}})).to eq([["bobsmith@example.com","sallyfield@example.com"],["555-555-5555","111-111-1111"],["Bob Smith","Sally Field"]])
+     end
+    
+    it "works on the empty hash [10 points]" , points: 10 do
+      expect { hash_2_array({}) }.not_to raise_error
+      expect(hash_2_array({})).to eq([[],[],[]])
+    end
+  end
+end
