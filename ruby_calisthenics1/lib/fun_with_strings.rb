2ca66e0f988e8b82803b67f3af3cc732bbd83bd4
Phillip Hirsch <phirsch@uncc.edu>
2019-06-03 02:14:01 +0000

    Lab 9: Fun With Words - anagram_groups

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -21,7 +21,31 @@ module FunWithStrings
   end
   def anagram_groups
     # your code here
+    words = self.split
+      results = Array.new
+
+      words.each_with_index do |myWord, index| 
+        unless results.any? { |arr| arr.include?(myWord) }
+
+          temp_array = Array.new
+          temp_array.push(myWord)
+
+          words[index+1..-1].each do |myWord2|
+
+            if myWord.is_anagram(myWord2)
+              temp_array.push(myWord2)
+            end
+          end
+          results.push(temp_array)
+        end
+    end
+    results
   end
+
+  def is_anagram(myWord)
+    self.downcase.chars.sort.join == myWord.downcase.chars.sort.join
+  end
+  
 end
 
 # make all the above functions available as instance methods on Strings:
