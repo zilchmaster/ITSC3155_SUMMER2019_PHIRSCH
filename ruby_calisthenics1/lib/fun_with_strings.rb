f11a9abf9ee3794eef23ae8dcbe71ad518bd35b1
Phillip Hirsch <phirsch@uncc.edu>
2019-06-03 00:58:29 +0000

    Lab 9: Fun With Words - Palindrome

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,6 +1,7 @@
 module FunWithStrings
   def palindrome?
     # your code here
+    self.gsub(/\W/,'').downcase == self.gsub(/\W/,'').downcase.reverse
   end
   def count_words
     # your code here
