661d54d3a0290fd0d4930bbe38432278492a97ee
Phillip Hirsch <phirsch@uncc.edu>
2019-06-03 22:42:34 +0000

    Ruby OO Basics - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,13 @@
 # Ruby Basics Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    # YOUR CODE HERE
+    attr_accessor :isbn
+    attr_accessor :price
+    
+    def initialize(isbn, price)
+        # your code here
+        @isbn = isbn
+        @price = price
+    end
 end
