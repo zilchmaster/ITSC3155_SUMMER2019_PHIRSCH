ecdb48f3bbfba2872f6b93d263350deb0123b720
Phillip Hirsch <phirsch@uncc.edu>
2019-06-16 23:51:15 +0000

    Add Delete functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -34,6 +34,12 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        
+        redirect_to articles_path
+    end
 end
 
 private
